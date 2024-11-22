6fd0b5f2555810352b032cfa3a794249ef641c89
NaafiRidho <naafiridho0505@gmail.com>
2024-11-22 17:12:06 +0700

    menambah cookies untuk user_id

--- proses_login.php
+++ proses_login.php
@@ -18,9 +18,12 @@ if (sqlsrv_execute($stmt)) {
     $role = null;
     while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
         $role = $row["role"];
+        $user_id=$row["user_id"];
     }
 
     if ($role == "Mahasiswa") {
+        setcookie("user_id",$user_id);
+
         header("Location: dashboardMhs.php");
         exit();
     }
