af9a0680e109e6233204f215c59c7a9f064b9714
NaafiRidho <naafiridho0505@gmail.com>
2024-11-24 14:18:29 +0700

    menambah untuk memasukkan admin berdasarkan prodi

--- aju_banding.php
+++ aju_banding.php
@@ -4,8 +4,14 @@ include "koneksi.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $pelaporan_id = $_POST['pelaporan_id'];
     $alasan_banding = $_POST['alasan_banding'];
+    $prodi = $_POST['prodi'];
+    $query = "";
 
-    $query = "INSERT INTO dbo.aju_banding VALUES (?,1,?,'Ditinjau',GETDATE())";
+    if ($prodi == 'SIB') {
+        $query = "INSERT INTO dbo.aju_banding VALUES (?,1,?,'Ditinjau',GETDATE())";
+    } else if ($prodi == "TI") {
+        $query = "INSERT INTO dbo.aju_banding VALUES (?,2,?,'Ditinjau',GETDATE())";
+    }
     $params = array($pelaporan_id, $alasan_banding);
     $stmt = sqlsrv_prepare($conn, $query, $params);
 
