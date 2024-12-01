0c63fe54e567f556212f3dc8facb279dd2be34c0
NaafiRidho <naafiridho0505@gmail.com>
2024-12-01 17:49:11 +0700

    untuk mengambil sanksi dari database

---
+++ get_sanksi.php
@@ -0,0 +1,25 @@
+<?php
+include 'koneksi.php'; // File koneksi ke SQL Server
+
+if (isset($_GET['tingkat_id'])) {
+    $tingkat_id = $_GET['tingkat_id'];
+
+    // Query untuk mengambil sanksi berdasarkan tingkat_id
+    $query = "SELECT sanksi FROM tingkat WHERE tingkat_id = ?";
+    $params = array($tingkat_id);
+    $stmt = sqlsrv_query($conn, $query, $params);
+
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    // Ambil data sanksi dari query
+    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+
+    if ($row) {
+        // Mengembalikan data dalam format JSON
+        echo json_encode(['sanksi' => $row['sanksi']]);
+    } else {
+        echo json_encode(['sanksi' => '']);
+    }
+}
