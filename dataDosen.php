c2bdaf81faef44b656a413089aa2166f8c3b2e9f
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2024-12-10 20:54:09 +0700

    benerin konflik

--- dataDosen.php
+++ dataDosen.php
@@ -13,7 +13,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <style>
-    body {
+    body { 
       margin: 0;
       font-family: Arial, Helvetica, sans-serif;
       background-color: #f9f9f9;
