06660d5e606c39a3313c40f4c3a0802e8251b342
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2024-12-29 16:18:59 +0700

    benerin tulisan list sanksi

--- listSanksiAdmin.php
+++ listSanksiAdmin.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Si Tertib - Data Dosen">
     <meta name="keywords" content="Data Dosen, Si Tertib">
-    <title>List Tata Tertib</title>
+    <title>List Sanksi</title>
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <!-- Bootstrap CSS -->
